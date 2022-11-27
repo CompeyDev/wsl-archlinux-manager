e4a01a315df4ba8cc14f2c72f3a735951215e0dd
Compey <hi@devcomp.xyz>
2022-11-27 17:53:16 +0530

    feat: untar progress bar & initial build mechanism

--- go.mod
+++ go.mod
@@ -6,12 +6,17 @@ require (
 	github.com/briandowns/spinner v1.19.0
 	github.com/cavaliergopher/grab/v3 v3.0.1
 	github.com/gookit/color v1.5.2
+	github.com/schollz/progressbar/v3 v3.12.1
 )
 
 require (
 	github.com/fatih/color v1.10.0 // indirect
 	github.com/mattn/go-colorable v0.1.8 // indirect
-	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/mattn/go-runewidth v0.0.14 // indirect
+	github.com/mitchellh/colorstring v0.0.0-20190213212951-d06e56a500db // indirect
+	github.com/rivo/uniseg v0.4.3 // indirect
 	github.com/xo/terminfo v0.0.0-20210125001918-ca9a967f8778 // indirect
-	golang.org/x/sys v0.0.0-20220503163025-988cb79eb6c6 // indirect
+	golang.org/x/sys v0.2.0 // indirect
+	golang.org/x/term v0.2.0 // indirect
 )
--- lib/core/core.go
+++ lib/core/core.go
@@ -1,20 +1,22 @@
 package core
 
 import (
+	"bufio"
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
+	"strconv"
 	"strings"
 	"time"
-
-	"encoding/json"
 
 	"github.com/CompeyDev/wsl-archlinux-manager/lib/checkers"
 	logger "github.com/CompeyDev/wsl-archlinux-manager/util"
 	"github.com/briandowns/spinner"
 	"github.com/cavaliergopher/grab/v3"
+	"github.com/schollz/progressbar/v3"
 )
 
 // TODO:
@@ -90,19 +92,81 @@ func Build() {
 		checkers.VerifySignature(mirror)
 	}
 
+	archiveName := fmt.Sprintf(`archlinux-bootstrap-%s-x86_64.tar.gz`, version)
+
+	untarArchive(archiveName, bar)
+
+	bar.Suffix = "  Building bootstrap package..."
+
+	bar.Start()
+
+	_d, buildErr := exec.Command("wsl.exe", "bash", "-c", "cd root.x86_64 && tar -zcvf arch_bootstrap_package.tar.gz .").CombinedOutput()
+
+	if buildErr != nil {
+		bar.Stop()
+		println(buildErr.Error())
+		println(string(_d))
+		logger.Error("Failed to build bootstrap package; fatal. Aborting installation...")
+	}
+	bar.Stop()
+	logger.Progress("Successfully built bootstrap package, proceeeding!")
+}
+
+func untarArchive(archiveName string, bar *spinner.Spinner) {
 	bar.Suffix = " Untarring archive..."
 	bar.Start()
 
-	stdoute, untarErr := exec.Command("wsl.exe", "bash", "-c", fmt.Sprintf(`tar -xzvf archlinux-bootstrap-%s-x86_64.tar.gz`, version)).CombinedOutput()
+	untarSizeCmd, sizeCmdErr := exec.Command("wsl.exe", "bash", "-c", fmt.Sprintf("tar -tzf %s | wc -l", archiveName)).CombinedOutput()
 
-	if untarErr != nil {
-		println(untarErr)
+	if sizeCmdErr != nil {
+		bar.Stop()
+		logger.Error("Failed to retrieve archive length; fatal. Refusing to continue.")
+	}
+
+	untarSizeArr := strings.Split(string(untarSizeCmd), "\n")
+	untarSizeStr := strings.Trim(untarSizeArr[len(untarSizeArr)-2], "\n\r ")
+	untarSize, convErr := strconv.ParseInt(untarSizeStr, 10, 64)
+
+	if convErr != nil {
+		bar.Stop()
+		logger.Error("An internal error occurred while attempting to untar the archive. Aborting.")
+	}
+
+	bar.Stop()
+
+	pb := progressbar.NewOptions(1000,
+		progressbar.OptionEnableColorCodes(true),
+		progressbar.OptionShowBytes(true),
+		progressbar.OptionSetPredictTime(true),
+		progressbar.OptionSetDescription("[yellow][1/3][reset] Untarring archive..."),
+		progressbar.OptionSetTheme(progressbar.Theme{
+			Saucer:        "=",
+			SaucerHead:    ">",
+			SaucerPadding: " ",
+			BarStart:      "[",
+			BarEnd:        "]",
+		}))
+
+	progressbar.DefaultBytes(untarSize)
+
+	untarCmd := exec.Command("wsl.exe", "bash", "-c", fmt.Sprintf("tar -xzvf %s", archiveName))
+
+	stdout, stdoutErr := untarCmd.StdoutPipe()
+	stderr, _ := untarCmd.StderrPipe()
+	_ = untarCmd.Start()
+
+	scanner := bufio.NewScanner(io.MultiReader(stdout, stderr))
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		pb.Add64(1)
+	}
+	_ = untarCmd.Wait()
+
+	if stdoutErr != nil {
 		logger.Error("Failed to untar archive; this is a non-recoverable error. Quitting.")
 	}
-	println(stdoute)
 	bar.Stop()
 	logger.Info("Successfully untarred archive!")
-
 }
 
 func getMirror(country string) string {
